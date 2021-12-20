e652363607f6ee42fe43b9d939d599010c1ccb83
Scott Stossel <stoss96@gmail.com>
2021-12-19 23:54:40 -0500

    shift, unshift, pop DLLs

--- linkedLists/doublyLinkedList.js
+++ linkedLists/doublyLinkedList.js
@@ -1,4 +1,4 @@
-class Node{
+class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
@@ -6,7 +6,7 @@ class Node{
   }
 }
 
-class DoublyLinkedList{
+class DoublyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
@@ -25,11 +25,74 @@ class DoublyLinkedList{
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    var poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    if (this.length === 0) return undefined;
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    if (index <= this.length / 2) {
+      var count = 0;
+      var current = this.head;
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      var count = this.length - 1;
+    }
+    return current;
+  }
 }
 
 var list = new DoublyLinkedList();
 list.push(99);
 list.push(100);
 list.push("Last Item");
+list.unshift("FIRST");
+// list.pop();
+// list.pop();
+// list.pop();
+// list.shift();
+// list.shift();
+// list.shift();
+// list.shift();
 
 console.log(list);
+console.log(list.get(1));
