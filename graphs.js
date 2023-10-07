a954e48ffc454016f6f645cc9bc1551e18b014aa
Scott Stossel <stoss96@gmail.com>
2023-10-07 16:24:15 +0200

    remove vertex and edge

--- graphs.js
+++ graphs.js
@@ -20,15 +20,31 @@ class Graph {
   addVertex(vertex) {
     if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
   }
-  addEdge(vertex1, vertex2) {
-    this.adjacencyList[vertex1].push(vertex2);
-    this.adjacencyList[vertex2].push(vertex1);
+  addEdge(v1, v2) {
+    this.adjacencyList[v1].push(v2);
+    this.adjacencyList[v2].push(v1);
+  }
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+      v => v !== v2
+    );
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+      v => v !== v1
+    );
+  }
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
   }
 }
 
 let g = new Graph();
 g.addVertex('Tokyo');
 g.addVertex('Zaragoza');
+g.addVertex('Palma');
 g.addVertex('SF');
 g.addVertex('Dallas');
 g.addVertex('Houston');
@@ -36,5 +52,10 @@ g.addVertex('Miami');
 g.addEdge('Tokyo', 'SF');
 g.addEdge('Dallas', 'Tokyo');
 g.addEdge('Dallas', 'Miami');
+g.addEdge('Dallas', 'Houston');
+g.addEdge('Zaragoza', 'Palma');
 
+console.log(g);
+
+g.removeVertex('Tokyo');
 console.log(g);
