78224a02c6f1aa9e73f782daed5a2d52881acb73
Scott Stossel <stoss96@gmail.com>
2023-10-10 00:49:35 +0200

    recursive depth traversal

--- graphs.js
+++ graphs.js
@@ -39,6 +39,23 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      })
+    })(start);
+    return result;
+  }
 }
 
 let g = new Graph();
@@ -81,3 +98,5 @@ graph.addEdge("C","E");
 graph.addEdge("D","E");
 graph.addEdge("D","F");
 graph.addEdge("E","F");
+
+console.log(graph.depthFirstRecursive("A"));
