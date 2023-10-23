bbf0179f6c3966eebe3b7faae878a3e365df44db
Scott Stossel <stoss96@gmail.com>
2023-10-22 23:34:04 -0400

    dijkstras algorithm

--- dijkstras.js
+++ dijkstras.js
@@ -18,6 +18,53 @@ class WeightedGraph {
     this.adjacencyList[vertex1].push({node: vertex2, weight});
     this.adjacencyList[vertex2].push({node: vertex1, weight});
   }
+  dijkstra(start, finish) {
+    const nodes = new PriorityQueue();
+    const distances = {};
+    const previous = {};
+    let path = [];
+    let smallest;
+    //build up initial state
+    for (let vertex in this.adjacencyList) {
+      if (vertex === start) {
+        distances[vertex] = 0;
+        nodes.enqueue(vertex, 0);
+      } else {
+        distances[vertex] = Infinity;
+        nodes.enqueue(vertex, Infinity);
+      }
+      previous[vertex] = null;
+    }
+    //as long as there is something to visit
+    while (nodes.values.length) {
+      smallest = nodes.dequeue().val;
+      if (smallest === finish) {
+        //done
+        while(previous[smallest]) {
+          path.push(smallest);
+          smallest = previous[smallest];
+        }
+        break;
+      }
+      if (smallest || distances[smallest] !== Infinity) {
+        for (let neighbor in this.adjacencyList[smallest]) {
+          let nextNode = this.adjacencyList[smallest][neighbor];
+          //new distance to node
+          let candidate = distances[smallest] + nextNode.weight;
+          let nextNeighbor = nextNode.node;
+          if (candidate < distances[nextNeighbor]) {
+            //updates new smallest distance to neighbor
+            distances[nextNeighbor] = candidate;
+            //updates path
+            previous[nextNeighbor] = smallest;
+            //enqueue in priority queue
+            nodes.enqueue(nextNeighbor, candidate);
+          }
+        }
+      }
+    }
+    return path.concat(smallest).reverse();
+  }
 }
 
 var graph = new WeightedGraph();
@@ -25,12 +72,20 @@ var graph = new WeightedGraph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
 
-graph.addEdge("A", "B", 9);
-graph.addEdge("A", "C", 5);
-graph.addEdge("B", "C", 7);
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
 
-console.log(graph.adjacencyList);
+// console.log(graph.adjacencyList);
 
 class PriorityQueue {
   constructor() {
@@ -50,9 +105,6 @@ class PriorityQueue {
 
 var q = new PriorityQueue();
 
-q.enqueue("B", 3);
-q.enqueue("C", 5);
-q.enqueue("D", 2);
-q.enqueue("Q", 20);
+// console.log(q.values);
 
-console.log(q.values);
+console.log(graph.dijkstra("A", "E"));
